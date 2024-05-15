2d6aa933e9893f52e31c97ad7dc7d00acfe5a37c
Joseph <118862484+Joseph-Meunier@users.noreply.github.com>
2024-05-15 11:04:15 +0200

    CRUDPlace Terminé

--- ProjetVeloBackEnd.API/Controllers/PlaceController.cs
+++ ProjetVeloBackEnd.API/Controllers/PlaceController.cs
@@ -9,7 +9,7 @@ namespace projet_velo_back_end.API.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-[Authorize]
+//[Authorize]
 public class PlaceController : Controller
 {
     private readonly ICRUDService<Place> _placeService;
@@ -24,6 +24,58 @@ public class PlaceController : Controller
     public async Task<IList<Place>> GetAll()
     {
         return await _placeService.GetAll();
+    }
+
+    // controller which displays a place by its id
+    [HttpGet("{id}")]
+    public async Task<Place?> Get(int id)
+    {
+        return await _placeService.Get(p => p.Id == id);
+    }
+    
+    // controller which adds a place to the database
+    [HttpPost]
+    public async Task<IActionResult> Add(Place place)
+    {
+        try
+        {
+            await _placeService.Insert(place);
+            return Ok();
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    // controller which updates a place in the database
+    [HttpPut]
+    public async Task<IActionResult> Update(Place place)
+    {
+        try
+        {
+            await _placeService.Update(place);
+            return Ok();
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    // controller which deletes a place from the database
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        try
+        {
+            await _placeService.Delete(id);
+            return Ok();
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
     }
 
 }
