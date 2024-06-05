46c06b24efd359bb7f93692a8deaa31481eacbef
Grifgrif13 <clement.bouilllot@gmail.com>
2024-06-05 11:22:16 +0200

    Add Authorize

--- ProjetVeloBackEnd.API/Controllers/IncidentController.cs
+++ ProjetVeloBackEnd.API/Controllers/IncidentController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using ProjetVeloBackEnd.Entities;
 using ProjetVeloBackEnd.Services.Contracts.Models;
 using ProjetVeloBackEnd.Services.Models;
@@ -7,6 +8,7 @@ namespace ProjetVeloBackEnd.API.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
+//[Authorize]
 public class IncidentController : Controller
 {
     private readonly IIncidentService _incidentService;
