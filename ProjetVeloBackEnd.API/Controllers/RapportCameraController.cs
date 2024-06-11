fd05bb364239b23ae00e7eb6b89dfcb5ae2caf7a
Joseph <118862484+Joseph-Meunier@users.noreply.github.com>
2024-06-11 09:56:39 +0200

    Création des endpoints getallrapportcamerabyidcamera et insert rapport camera

---
+++ ProjetVeloBackEnd.API/Controllers/RapportCameraController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetVeloBackEnd.Entities;
+using ProjetVeloBackEnd.Services.Contracts.DTO.Down;
+using ProjetVeloBackEnd.Services.Contracts.DTO.Up;
+using ProjetVeloBackEnd.Services.Contracts.Models;
+
+namespace ProjetVeloBackEnd.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+//[Authorize]
+public class RapportCameraController : Controller
+{
+    private readonly IRapportCamera _rapportCameraService;
+
+    public RapportCameraController(IRapportCamera rapportCameraService)
+    {
+        _rapportCameraService = rapportCameraService;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IList<RapportCameraDtoUp>> GetAllByIdCamera(int id)
+    {
+        return await _rapportCameraService.GetRapportCameraByIdCamera(id);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Insert(RapportCameraRegisterDtoDown rapportCameraDtoUp)
+    {
+        await _rapportCameraService.InsertRapportCamera(rapportCameraDtoUp);
+        return Ok();
+    }
+}
---
+++ ProjetVeloBackEnd.DAL/Migrations/20240611075116_createRapportCamera.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjetVeloBackEnd.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class createRapportCamera : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RapportCamera",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdCamera = table.Column<int>(type: "int", nullable: false),
+                    GaucheADroite = table.Column<int>(type: "int", nullable: false),
+                    DroiteAGauche = table.Column<int>(type: "int", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RapportCamera", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RapportCamera");
+        }
+    }
+}
